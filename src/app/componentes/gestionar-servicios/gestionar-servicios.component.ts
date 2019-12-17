96d34be7e767ea9b69e83b18ca3f65c1da6153d4
mario.riguera <mario.riguera@servac.avianet.cu>
2019-12-17 14:19:36 -0500

    Trabajando en el buscador de la tabla de Gestionar Servicios

--- src/app/componentes/gestionar-servicios/gestionar-servicios.component.ts
+++ src/app/componentes/gestionar-servicios/gestionar-servicios.component.ts
@@ -1,136 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
+import { MdbTableDirective } from 'angular-bootstrap-md';
 
 @Component({
   selector: 'app-gestionar-servicios',
   templateUrl: './gestionar-servicios.component.html',
   styleUrls: ['./gestionar-servicios.component.scss']
 })
-export class GestionarServiciosComponent implements OnInit {
-
-  elements: any = [
-    {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
-    {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
-    {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
-  ];
-
-  headElements = ['ID', 'First', 'Last', 'Handle'];
-
-
-
+export class GestionarServiciosComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-}
-
-/* import { Component, OnInit, ElementRef, HostListener, AfterViewInit, ViewChild, ChangeDetectorRef } from '@angular/core';
-import { MdbTableDirective, MdbTablePaginationComponent } from 'ng-uikit-pro-standard';
-@Component({
-  selector: 'app-gestionar-servicios',
-  templateUrl: './gestionar-servicios.component.html',
-  styleUrls: ['./gestionar-servicios.component.scss']
-})
-export class GestionarServiciosComponent implements OnInit, AfterViewInit {
-  @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
-  @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
-  @ViewChild('row', { static: true }) row: ElementRef;
-
+  @ViewChild(MdbTableDirective, {static: true}) mdbTable: MdbTableDirective;
   elements: any = [];
-  headElements = ['id', 'first', 'last', 'handle'];
-
-  searchText: string = '';
+  headElements = ['Nombre de la Persona'	, 'Edad' ,	'Compañía' , 'Nombre de la Ciudad'	, 'Ciudad' , 'Eliminar'];
+  searchText = '';
   previous: string;
 
-  maxVisibleItems: number = 8;
+  // tslint:disable-next-line:member-ordering
+  editField: string;
+    // tslint:disable-next-line:member-ordering
+    personList: Array<any> = [
+      { id: 1, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
+      { id: 2, name: 'Guerra Cortez', age: 45, companyName: 'Insectus', country: 'USA', city: 'San Francisco' },
+      { id: 3, name: 'Guadalupe House', age: 26, companyName: 'Isotronic', country: 'Germany', city: 'Frankfurt am Main' },
+      { id: 4, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
+      { id: 5, name: 'Elisa Gallagher', age: 31, companyName: 'Portica', country: 'United Kingdom', city: 'London' },
+    ];
 
-  constructor(private cdRef: ChangeDetectorRef) {}
+    awaitingPersonList: Array<any> = [
+      { id: 6, name: 'George Vega', age: 28, companyName: 'Classical', country: 'Russia', city: 'Moscow' },
+      { id: 7, name: 'Mike Low', age: 22, companyName: 'Lou', country: 'USA', city: 'Los Angeles' },
+      { id: 8, name: 'John Derp', age: 36, companyName: 'Derping', country: 'USA', city: 'Chicago' },
+      { id: 9, name: 'Anastasia John', age: 21, companyName: 'Ajo', country: 'Brazil', city: 'Rio' },
+      { id: 10, name: 'John Maklowicz', age: 36, companyName: 'Mako', country: 'Poland', city: 'Bialystok' },
+    ];
 
   @HostListener('input') oninput() {
-    this.mdbTablePagination.searchText = this.searchText;
+    this.searchItems();
   }
 
   ngOnInit() {
-    for (let i = 1; i <= 25; i++) {
-      this.elements.push({id: i.toString(), first: 'Wpis ' + i, last: 'Last ' + i, handle: 'Handle ' + i});
-    }
-
-    this.mdbTable.setDataSource(this.elements);
-    this.elements = this.mdbTable.getDataSource();
-    this.previous = this.mdbTable.getDataSource();
-  }
-
-  ngAfterViewInit() {
-    this.mdbTablePagination.setMaxVisibleItemsNumberTo(this.maxVisibleItems);
-
-    this.mdbTablePagination.calculateFirstItemIndex();
-    this.mdbTablePagination.calculateLastItemIndex();
-    this.cdRef.detectChanges();
-  }
-
-  addNewRow() {
-    this.mdbTable.addRow({
-      id: this.elements.length.toString(),
-      first: 'Wpis ' + this.elements.length,
-      last: 'Last ' + this.elements.length,
-      handle: 'Handle ' + this.elements.length
-    });
-    this.emitDataSourceChange();
-  }
-
-  addNewRowAfter() {
-    this.mdbTable.addRowAfter(1, {id: '2', first: 'Nowy', last: 'Row', handle: 'Kopytkowy'});
-    this.mdbTable.getDataSource().forEach((el: any, index: any) => {
-      el.id = (index + 1).toString();
-    });
-    this.emitDataSourceChange();
-  }
-
-  removeLastRow() {
-    this.mdbTable.removeLastRow();
-    this.emitDataSourceChange();
-    this.mdbTable.rowRemoved().subscribe((data: any) => {
-      console.log(data);
-    });
-  }
-
-  removeRow() {
-    this.mdbTable.removeRow(1);
-    this.mdbTable.getDataSource().forEach((el: any, index: any) => {
-      el.id = (index + 1).toString();
-    });
-    this.emitDataSourceChange();
-    this.mdbTable.rowRemoved().subscribe((data: any) => {
-      console.log(data);
-    });
-  }
-
-  emitDataSourceChange() {
-    this.mdbTable.dataSourceChange().subscribe((data: any) => {
-      console.log(data);
-    });
+/*     for (let i = 1; i <= 10; i++) {
+		  this.elements.push({ id:i.toString(), first: 'Wpis' + (Math.floor(Math.random() * i * 10)).toString(), last: 'Last' + (Math.floor(Math.random() * i *10)).toString(), handle: 'Handle' + (Math.floor(Math.random() * i *10)).toString() });
+		}
+		  this.mdbTable.setDataSource(this.elements);
+		  this.previous = this.mdbTable.getDataSource(); */
   }
 
   searchItems() {
-    const prev = this.mdbTable.getDataSource();
+/* 		const prev = this.mdbTable.getDataSource(); if (!this.searchText) {
+		this.mdbTable.setDataSource(this.previous); this.elements = this.mdbTable.getDataSource(); }
+		if (this.searchText) {
+		this.elements =this.mdbTable.searchLocalDataByMultipleFields(this.searchText, ['first','last']);
+		this.mdbTable.setDataSource(prev);
+		} */
+	}
 
-    if (!this.searchText) {
-      this.mdbTable.setDataSource(this.previous);
-      this.elements = this.mdbTable.getDataSource();
+    updateList(id: number, property: string, event: any) {
+      const editField = event.target.textContent;
+      this.personList[id][property] = editField;
     }
 
-    if (this.searchText) {
-      this.elements = this.mdbTable.searchLocalDataBy(this.searchText);
-      this.mdbTable.setDataSource(prev);
+    remove(id: any) {
+      this.awaitingPersonList.push(this.personList[id]);
+      this.personList.splice(id, 1);
     }
 
-    this.mdbTablePagination.calculateFirstItemIndex();
-    this.mdbTablePagination.calculateLastItemIndex();
+    add() {
+      if (this.awaitingPersonList.length > 0) {
+        const person = this.awaitingPersonList[0];
+        this.personList.push(person);
+        this.awaitingPersonList.splice(0, 1);
+      }
+    }
 
-    this.mdbTable.searchDataObservable(this.searchText).subscribe(() => {
-      this.mdbTablePagination.calculateFirstItemIndex();
-      this.mdbTablePagination.calculateLastItemIndex();
-    });
-  }
-} */
+    changeValue(id: number, property: string, event: any) {
+      this.editField = event.target.textContent;
+    }
+
+}
+
+
